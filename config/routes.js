07c928edebbb70f75f59f20acb56f8865e43758a
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-04 22:57:36 +0300

    added routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,14 @@
-// TODO: Require Controllers...
+const cubeControllers = require('../controllers/cube');
 
 module.exports = (app) => {
-    // TODO...
+    app.get('/', cubeControllers.getCubes),
+    app.get('/details/:id', cubeControllers.getCube),
+    app.post('/create', cubeControllers.createCubePost),
+    app.get('/create', cubeControllers.createCubeGet),
+    app.get('/about', function(req, res){
+        res.render('about', {layout: false});
+    }),
+    app.get('*', function(req,res){
+        res.render('404', {layout: false})
+    })
 };
