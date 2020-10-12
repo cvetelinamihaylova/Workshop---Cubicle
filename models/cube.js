b0089db602ca158ca84712395442869d35524af5
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-12 20:57:50 +0300

    refactored code for mongoose schema

--- models/cube.js
+++ models/cube.js
@@ -1,29 +1,14 @@
-const path = require('path');
+const mongoose = require('mongoose');
 
-const BaseModel = require('./base');
+const cubeSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    imageURL: String,
+    difficultyLevel: Number,
+    accessories: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Accessory'
+    }]
+});
 
-class CubeModel extends BaseModel {
-
-    constructor() {
-        const filePath = path.join(global._basedir, '/config/database.json');
-        super(filePath);
-
-    }
-
-    insert(name, description, imageURL, difficultyLevel) {
-
-        return super.insert({ name, description, imageURL, difficultyLevel });
-    }
-
-    getAll(query) {
-        if (!query) { return super.getAll() }
-        const {name, from, to} = query;
-        return super.queryBy(function(entry){
-            return (name ? entry.name.includes(name) : true) &&
-            (from ? entry.difficultyLevel >= from : true) &&
-            (to ? entry.difficultyLevel <= to : true)
-        })
-    }
-}
-
-module.exports = new CubeModel();
+module.exports = new mongoose.model('Cube', cubeSchema);
