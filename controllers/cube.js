4944ff177de4f572f36bb36989ad310194772d44
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-12 20:56:44 +0300

    refactored code for mongoose model

--- controllers/cube.js
+++ controllers/cube.js
@@ -2,26 +2,43 @@ const cubeModel = require('../models/cube');
 
 module.exports = {
     getCubes(req, res, next) {
-        const { from, search, to} = req.query;
-        cubeModel.getAll({name: search, from: +from, to: +to}).then(cubes => {
-            res.render('index', { layout: false, cubes, from, search, to });
-        })
+        const { from, search, to } = req.query;
+        const query = {};
+        if (search) { query.name = new RegExp(search, 'i') }
+        if (from) {
+            query.difficultyLevel = { $gte: +from }
+        }
+        if (to) {
+            query.difficultyLevel = query.difficultyLevel || {};
+            query.difficultyLevel = { $lte: +to }
+        }
+
+        cubeModel.find(query)
+            .populate('accessories')
+            .then((cubes) => {
+                res.render('index', { cubes, from, search, to });
+            })
             .catch(next);
     },
     getCube(req, res, next) {
-        const id = +req.params.id;
-        cubeModel.findById(id).then(cube => {
-            res.render('details', { layout: false, cube })
-        })
+        const id = req.params.id;
+        cubeModel.findById(id)
+            .populate('accessories')
+            .then(cube => {
+                res.render('details', { cube })
+            })
             .catch(next);
     },
     createCubePost(req, res, next) {
         const { name, description, imageURL, difficultyLevel } = req.body;
-        cubeModel.insert(name, description, imageURL, +difficultyLevel)
-            .then(() => res.redirect('/'))
+
+        cubeModel.create({ name, description, imageURL, difficultyLevel: +difficultyLevel })
+            .then(() => {
+                res.redirect('/');
+            })
             .catch(next);
     },
     createCubeGet(req, res, next) {
-        res.render('create', { layout: false })
+        res.render('create')
     }
 }
