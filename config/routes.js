6fb999c4a005782ede0e0ae0ed6ff410ec5a3484
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-12 20:54:55 +0300

    added accessory routes

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,23 @@
 const cubeControllers = require('../controllers/cube');
+const accessoryControllers = require('../controllers/accessory');
+
 
 module.exports = (app) => {
     app.get('/', cubeControllers.getCubes),
     app.get('/details/:id', cubeControllers.getCube),
+
+    app.get('/create/accessory', accessoryControllers.createAccessoryGet),
+    app.post('/create/accessory', accessoryControllers.createAccessoryPost),
+
+    app.get('/attach/accessory/:id', accessoryControllers.attachAccessoryGet),
+    app.post('/attach/accessory/:id', accessoryControllers.attachAccessoryPost),
+
     app.post('/create', cubeControllers.createCubePost),
     app.get('/create', cubeControllers.createCubeGet),
     app.get('/about', function(req, res){
-        res.render('about', {layout: false});
+        res.render('about');
     }),
     app.get('*', function(req,res){
-        res.render('404', {layout: false})
+        res.render('404')
     })
 };
