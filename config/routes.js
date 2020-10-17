3e8bb2117fa6a183327b0a8407fa7b070ed670dc
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-17 23:11:41 +0300

    added login and register routes

--- config/routes.js
+++ config/routes.js
@@ -1,23 +1,30 @@
 const cubeControllers = require('../controllers/cube');
 const accessoryControllers = require('../controllers/accessory');
+const userControllers = require('../controllers/user');
+const checkAuth = require('../middlewares/check-auth');
 
 
 module.exports = (app) => {
     app.get('/', cubeControllers.getCubes),
-    app.get('/details/:id', cubeControllers.getCube),
+        app.get('/login', checkAuth(false), userControllers.getLogin),
+        app.get('/register', checkAuth(false), userControllers.getRegister),
+        app.post('/register', checkAuth(false), userControllers.postRegister),
+        app.post('/login', checkAuth(false), userControllers.postLogin),
 
-    app.get('/create/accessory', accessoryControllers.createAccessoryGet),
-    app.post('/create/accessory', accessoryControllers.createAccessoryPost),
+        app.get('/details/:id', cubeControllers.getCube),
 
-    app.get('/attach/accessory/:id', accessoryControllers.attachAccessoryGet),
-    app.post('/attach/accessory/:id', accessoryControllers.attachAccessoryPost),
+        app.get('/create/accessory', checkAuth(true), accessoryControllers.createAccessoryGet),
+        app.post('/create/accessory', checkAuth(true), accessoryControllers.createAccessoryPost),
 
-    app.post('/create', cubeControllers.createCubePost),
-    app.get('/create', cubeControllers.createCubeGet),
-    app.get('/about', function(req, res){
-        res.render('about');
-    }),
-    app.get('*', function(req,res){
-        res.render('404')
-    })
+        app.get('/attach/accessory/:id', checkAuth(true), accessoryControllers.attachAccessoryGet),
+        app.post('/attach/accessory/:id', checkAuth(true), accessoryControllers.attachAccessoryPost),
+
+        app.post('/create', checkAuth(true), cubeControllers.createCubePost),
+        app.get('/create', checkAuth(true), cubeControllers.createCubeGet),
+        app.get('/about', function (req, res) {
+            res.render('about');
+        }),
+        app.get('*', function (req, res) {
+            res.render('404')
+        })
 };
