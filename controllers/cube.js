82869b82733a3eda3daacbd7505ee2feb7085be9
Cvetelina Mihaylova <cvetelinapm@abv.bg>
2020-10-18 22:15:39 +0300

    addet post and delete controllers

--- controllers/cube.js
+++ controllers/cube.js
@@ -16,7 +16,7 @@ module.exports = {
         cubeModel.find(query)
             .populate('accessories')
             .then((cubes) => {
-                res.render('index', { cubes, from, search, to} );
+                res.render('index', { cubes, from, search, to });
             })
             .catch(next);
     },
@@ -25,11 +25,11 @@ module.exports = {
         cubeModel.findById(id)
             .populate('accessories')
             .then(cube => {
-                res.render('details', { cube})
+                res.render('details', { cube })
             })
             .catch(next);
     },
-    createCubePost(req, res, next) {
+    postCreateCube(req, res, next) {
         const { name, description, imageURL, difficultyLevel } = req.body;
 
         cubeModel.create({ name, description, imageURL, difficultyLevel: +difficultyLevel })
@@ -38,7 +38,42 @@ module.exports = {
             })
             .catch(next);
     },
-    createCubeGet(req, res, next) {
+    getCreateCube(req, res, next) {
         res.render('create')
+    },
+    postEditCube(req, res, next) {
+        const id = req.params.id;
+        const { name, description, imageURL, difficultyLevel } = req.body;
+
+        cubeModel.update({ _id: id }, { name, description, imageURL, difficultyLevel: +difficultyLevel })
+            .then(() => {
+                res.redirect('/');
+            })
+            .catch(next);
+    },
+    getEditCube(req, res, next) {
+        const id = req.params.id;
+        cubeModel.findById(id)
+            .then(cube => {
+                res.render('edit-cube', cube)
+            })
+            .catch(next);
+    },
+    getDeleteCube(req, res, next) {
+        const id = req.params.id;
+        cubeModel.findById(id)
+            .then(cube => {
+                res.render('delete-cube', cube)
+            })
+            .catch(next);
+    },
+    postDeleteCube(req, res, next) {
+        const id = req.params.id;
+
+        cubeModel.deleteOne({ _id: id })
+            .then(() => {
+                res.redirect('/');
+            })
+            .catch(next);
     }
-}
+};
